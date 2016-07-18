7f1267e3259a47948c7e011f83c5f4898f353bc9
Cruz Barnum <cruzbarnum@gmail.com>
2016-07-18 03:12:07 -0700

    Eliminated some of the bias toward the right in the Dijkstra Pathfinding algorithm

--- src/game/PathFinding.java
+++ src/game/PathFinding.java
@@ -23,30 +23,35 @@ public class PathFinding {
     private Vec2 goal;
     private int width;
     private int height;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Terrain t = (new MapGen(100).generate(40, 40));
         double[][] speedMap = t.getSpeedMap();
-        PathFinding p = new PathFinding(speedMap,new Vec2(10,11));
-        for(int j = 0; j < 20; j++){
-            for(int i = 0; i < 20; i++){
-                if(i == 10 && j == 11){
+        PathFinding p = new PathFinding(speedMap, new Vec2(10, 11));
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 20; i++) {
+                if (i == 10 && j == 11) {
                     System.out.print("@");
-                }
-                else{
+                } else {
                     System.out.print(speedMap[i][j] > 0 ? "." : "#");
                 }
-                
+
             }
             System.out.println();
         }
-        for(int j = 0; j < 20; j++){
-            for(int i = 0; i < 20; i++){
-                System.out.printf("[% .1f]", p.distMap[i][j]);
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 20; i++) {
+                if(p.distMap[i][j] != Double.POSITIVE_INFINITY)
+                System.out.printf("[% 3.0f]", p.distMap[i][j]);
+                else{
+                    System.out.print("[inf]");
+                }
             }
             System.out.println();
         }
-        
+
     }
+
     public PathFinding(double[][] speedMap, Vec2 goal) {
 
         this.speedMap = toNonDestructableMap(speedMap);
@@ -74,18 +79,34 @@ public class PathFinding {
     private double[][] generateDistanceMap() {
 
         Vec2[] nl = new Vec2[width * height - 1];
-        nl[0] = goal.add(new Vec2(goal.x == 0 ? 1 : -1, 0));
-        int index = 1;
+        int index = 0;
 
         double[][] dm = new double[width][height];
         dm[(int) goal.x][(int) goal.y] = 1;
 
+        for (int i = 0; i < 7; i++) {
+
+            if (i != 0 && i != 2 && i != 5) {
+
+                Vec2 adj = goal.add(new Vec2(ADX[i], ADY[i]));
+
+                if (adj.x < width && adj.y < height && adj.x >= 0 && adj.y >= 0) {
+
+                    dm[(int) adj.x][(int) adj.y] = -2;
+                    nl[index] = adj;
+                    index++;
+                }
+            }
+        }
+
         for (int i = 0; i < width * height - 1; i++) {
 
             Vec2 t = nl[i];
-            if(t == null) break;
+            if (t == null) {
+                break;
+            }
             double ss = Double.POSITIVE_INFINITY;
-            if(speedMap[(int)t.x][(int)t.y] != Double.POSITIVE_INFINITY){
+            if (speedMap[(int) t.x][(int) t.y] != Double.POSITIVE_INFINITY) {
                 for (int j = 0; j < 8; j++) {
 
                     Vec2 adj = t.add(new Vec2(ADX[j], ADY[j]));
@@ -107,7 +128,7 @@ public class PathFinding {
                 }
             }
             dm[(int) t.x][(int) t.y] = ss + speedMap[(int) t.x][(int) t.y];
-            
+
         }
 
         return dm;
